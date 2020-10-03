28e224a4f8fcd6b5c833f5b605429a64f97fec3e
Artur <54704039+Artur-I@users.noreply.github.com>
2020-10-03 16:59:56 +0300

    feat: fill in solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof array !== 'undefined' && array.length > 0) {
+        return Math.min(...array)
+    }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array !== 'undefined' && array.length > 0) {
+        return Math.max(...array)
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let sum = array.reduce((acc, rec) => acc + rec)
+        return sum / array.length
+    }
+    return 0;
 }
